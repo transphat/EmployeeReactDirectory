b56240bd1dddd8acda87e27140fd44219cd1a858
Dung Tran <trankhadung@gmail.com>
2021-01-20 22:29:46 -0500

    first commit

---
+++ build/precache-manifest.5d833210a70eb195b3b524e02f438ef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200b9c784a8d12581ce1c6cd188f7ad8",
+    "url": "/EmployeeDirectory/index.html"
+  },
+  {
+    "revision": "7252f5319e3623bdce25",
+    "url": "/EmployeeDirectory/static/css/main.42bb3c76.chunk.css"
+  },
+  {
+    "revision": "dbc59f43f6b7bca6794b",
+    "url": "/EmployeeDirectory/static/js/2.b3c947f5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/EmployeeDirectory/static/js/2.b3c947f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7252f5319e3623bdce25",
+    "url": "/EmployeeDirectory/static/js/main.ae15ddeb.chunk.js"
+  },
+  {
+    "revision": "eff38c4d06d9ff961c4e",
+    "url": "/EmployeeDirectory/static/js/runtime-main.d3b68ede.js"
+  }
+]);
